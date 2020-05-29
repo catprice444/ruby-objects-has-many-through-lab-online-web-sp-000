70abfcc00dd958051b021e1535b1741aaa142f7a
Catherine Price <catprice444@gmail.com>
2020-05-29 21:08:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,10 +12,8 @@ class Genre
     @@all
   end
 
-  def songs #iterates through all songs and finds the song that belong to that genre
-    Song.all.find do |genre|
-      genre == Song.all
-    end
+  def songs #iterates through all songs and select the song that belong to that genre
+    Song.all.select {|song| song.genre == self}
   end
 
   def artists #iterates over the genre's collection of songs and collects the artist that owns each song
