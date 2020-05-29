ea3a0453df8f799083642da35268988bc5ba4620
Catherine Price <catprice444@gmail.com>
2020-05-29 21:22:54 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,7 @@ class Patient
   end
 
   def appointments #iterates through the Appointments array and selects Appointments that belong to the patient
-    Appointments.all.select {|appointment| appointment.patient == self}
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
 
   def doctors #iterates over that patient's Appointments and maps the doctor that belongs to each Appointment
