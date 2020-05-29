bef2dfb67c5c6977be1c4ba79ab5c4f3ea8c60db
Catherine Price <catprice444@gmail.com>
2020-05-29 21:16:29 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
 
   def new_appointment #takes in a date and an instance of the Doctor class and creates a new Appointment.
                       #the Appointment should know that is belongs to the patient
-
+    Appointment.new(date, self, doctor)                  
   end
 
   def appointments #iterates through the Appointments array and selects Appointments that belong to the patient
